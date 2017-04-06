998ea8df051ab6ab503fd1ff93536a56fc0a03c1
luigivieira <luigivieira@gmail.com>
2017-04-06 13:45:40 -0300

    Primeiro commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Classe principal do controlador do jogo.
+/// </summary>
+public class GameController: MonoBehaviour
+{
+	/// <summary>
+    /// Método de inicialização do jogo (chamado uma vez no início).
+    /// </summary>
+	void Start()
+    {
+		
+	}
+	
+	/// <summary>
+    /// Método de execução do jogo (chamado a cada quadro).
+    /// </summary>
+	void Update()
+    {
+        // A tecla "escape" funciona como a tecla "voltar" nos dispositivos móveis
+        if(Input.GetKey("escape"))
+            terminate();
+	}
+
+    /// <summary>
+    /// Realiza o encerramento do jogo (via tecla "voltar" ou via UI).
+    /// Por segurança, o usuário é solicitado a confirmar o encerramento.
+    /// </summary>
+    public void terminate()
+    {
+        // TODO: Questionar encerramento
+        Application.Quit();
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Mobile Painter
+==============
+
+Aplicação simples de desenho para dispositivos móveis, criada em Unity3D. Copyright (c) 2017 - [Luiz Carlos Vieira](http://www.luiz.vieira.nom.br).

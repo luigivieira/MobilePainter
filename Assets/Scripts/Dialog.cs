1f07c0f92d4740ea4e956a29808ee3ac8876ede8
luigivieira <luigivieira@gmail.com>
2017-04-06 19:42:16 -0300

    Telas de diálogo e informação

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+/// <summary>
+/// Singleton que implementa o acesso aos diálogos de interação com o usuário.
+/// </summary>
+public class Dialog : MonoBehaviour
+{
+    /// <summary>
+    /// Objeto do diálogo na UI.
+    /// </summary>
+    public GameObject dialogObject;
+
+    /// <summary>
+    /// Objeto com o texto do diálogo.
+    /// </summary>
+    public Text textObject;
+
+    /// <summary>
+    /// Objeto do botão "Sim" do diálogo.
+    /// </summary>
+    public Button yesButton;
+
+    /// <summary>
+    /// Objeto com o botão "Não" do diálogo.
+    /// </summary>
+    public Button noButton;
+
+    private static Dialog defaultInstance;
+
+    /// <summary>
+    /// Método estático de acesso à instância única do singleton.
+    /// </summary>
+    /// <returns>Retorna a instância única da classe <c>Dialog</c>.</returns>
+    public static Dialog instance()
+    {
+        if(!defaultInstance)
+        {
+            defaultInstance = FindObjectOfType(typeof(Dialog)) as Dialog;
+            if (!defaultInstance)
+                Debug.LogError("Oops! Deve existir uma instância singleton da classe Dialog!");
+        }
+        return defaultInstance;
+    }
+
+    /// <summary>
+    /// Apresenta o diálogo modal para o usuário com o texto dado e com os botões "Sim" e "Não".
+    /// </summary>
+    /// <param name="text">String com o texto a ser apresentado na janela de diálogo.</param>
+    /// <param name="yesEvent">Instância de <c>UnityAction</c> com o evento a ser chamado no
+    /// clique do botão "Sim".</param>
+    /// <param name="noEvent">Instância de <c>UnityAction</c> com o evento a ser chamado no
+    /// clique do botão "Não".</param>
+	public void askYesNo(string text, UnityAction yesEvent, UnityAction noEvent)
+    {
+        yesButton.onClick.AddListener(yesEvent);
+        yesButton.onClick.AddListener(close);
+        
+        noButton.onClick.AddListener(noEvent);
+        noButton.onClick.AddListener(close);
+
+        textObject.text = text;
+
+        yesButton.enabled = true;
+        noButton.enabled = true;
+        dialogObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Fecha o painel com o diálogo modal.
+    /// </summary>
+    void close()
+    {
+        textObject.text = "";
+
+        yesButton.onClick.RemoveAllListeners();
+        noButton.onClick.RemoveAllListeners();
+
+        yesButton.enabled = false;
+        noButton.enabled = false;
+
+        dialogObject.SetActive(false);
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,24 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 /// <summary>
 /// Classe principal do controlador do jogo.
 /// </summary>
 public class GameController: MonoBehaviour
 {
+    /// <summary>
+    /// Painel de exibição de informações sobre a aplicação.
+    /// </summary>
+    public GameObject infoPanel;
+
+    /// <summary>
+    /// Instância única do objeto de diálogo com o usuário.
+    /// </summary>
+    private Dialog dialog;
+
+    /// <summary>
+    /// Objeto de ação para o botão de confirmação do diálogo de encerramento da aplicação.
+    /// </summary>
+    private UnityAction confirmExit;
+
+    /// <summary>
+    /// Objeto de ação para o botão de negação do diálogo de encerramento da aplicação.
+    /// </summary>
+    private UnityAction ignoreExit;
+
 	/// <summary>
     /// Método de inicialização do jogo (chamado uma vez no início).
     /// </summary>
 	void Start()
     {
-		
-	}
-	
-	/// <summary>
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        dialog = Dialog.instance();
+        Debug.Log("Oi");
+        Debug.Log(dialog);
+        confirmExit = new UnityAction(quit);
+        ignoreExit = new UnityAction(ignore);
+    }
+
+    /// <summary>
     /// Método de execução do jogo (chamado a cada quadro).
     /// </summary>
-	void Update()
+    void Update()
     {
         // A tecla "escape" funciona como a tecla "voltar" nos dispositivos móveis
         if(Input.GetKey("escape"))
@@ -26,12 +52,35 @@ public class GameController: MonoBehaviour
 	}
 
     /// <summary>
-    /// Realiza o encerramento do jogo (via tecla "voltar" ou via UI).
-    /// Por segurança, o usuário é solicitado a confirmar o encerramento.
+    /// Exibe ou esconde a tela com as informações sobre a aplicação.
+    /// </summary>
+    public void toggleInfo()
+    {
+        infoPanel.SetActive(!infoPanel.activeSelf);
+    }
+
+    /// <summary>
+    /// Realiza o encerramento do jogo (via tecla "voltar" ou via UI), solicitando ao
+    /// usuário a confirmação por meio de uma tela de diálogo.
     /// </summary>
     public void terminate()
     {
-        // TODO: Questionar encerramento
+        dialog.askYesNo("Você tem certeza que deseja fechar a aplicação?", confirmExit, ignoreExit);
+    }
+
+    /// <summary>
+    /// Fecha a aplicação e retorna para o sistema operacional.
+    /// </summary>
+    protected void quit()
+    {
         Application.Quit();
+    }
+
+    /// <summary>
+    /// Método "dummy" utilizado pelo botão "Não" no diálogo de encerramento da aplicação
+    /// (já que nessa ação a aplicação não precisa fazer nada).
+    /// </summary>
+    protected void ignore()
+    {
     }
 }
